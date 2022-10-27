ba5a085e81902cc2b8dac6652acf910dea077bb0
ThanhUITK12 <phuocthanh99@gmail.com>
2022-10-28 00:55:07 +0700

    create QuestionBrick with basic func

---
+++ 05-SceneManager/QuestionBrick.h
@@ -0,0 +1,26 @@
+#pragma once
+#pragma once
+#include "GameObject.h"
+
+
+
+
+class CQuestionBrick : public CGameObject
+{
+protected:
+
+
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void Render();
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+	virtual void OnNoCollision(DWORD dt);
+
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+
+public:
+	CQuestionBrick(float x, float y);
+	virtual void SetState(int state);
+};
