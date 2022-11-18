7185b33e46d9eb9ba8e274bb553cfb96d7e76645
ThanhUITK12 <phuocthanh99@gmail.com>
2022-11-18 21:49:23 +0700

    define value for koopa

--- 05-SceneManager/Koopa.h
+++ 05-SceneManager/Koopa.h
@@ -2,6 +2,20 @@
 
 #include "GameObject.h"
 
+#define KOOPA_STATE_NORMAL 100
+#define KOOPA_BBOX_WIDTH 16
+#define KOOPA_BBOX_HEIGHT 26
+#define KOOPA_WALK_RIGHT_ANI_ID 0
+#define KOOPA_SPIN_RIGHT_ANI_ID 1
+#define KOOPA_WALK_LEFT_ANI_ID 2
+#define KOOPA_SPIN_LEFT_ANI_ID 3
+#define KOOPA_SHELL_ANI_ID 4
+#define KOOPA_SHAKE_ANI_ID 5
+#define KOOPA_DIE_ANI_ID 6
+#define KOOPA_PARA_RIGHT_ANI_ID 7
+#define KOOPA_PARA_LEFT_ANI_ID 8
+#define KOOPA_SHAKE_UP_ANI_ID 9
+
 
 class CKoopa : public CGameObject
 {
