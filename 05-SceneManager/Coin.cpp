fe5899d6fd7816fc878f6f39814216f372934c66
ThanhUITK12 <phuocthanh99@gmail.com>
2022-12-11 03:58:32 +0700

    fix bbox coin

--- 05-SceneManager/Coin.cpp
+++ 05-SceneManager/Coin.cpp
@@ -19,8 +19,8 @@ void CCoin::Render()
 
 void CCoin::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
-	l = x;
-	t = y;
+	l = x - COIN_BBOX_WIDTH/2;
+	t = y - COIN_BBOX_HEIGHT/2;
 	r = l + COIN_BBOX_WIDTH;
 	b = t + COIN_BBOX_HEIGHT;
 }
