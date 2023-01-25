5b3f8a0984230630e6b5493a22b8affa49c04aa9
ThanhUITK12 <phuocthanh99@gmail.com>
2023-01-25 21:45:41 +0700

    create card class

---
+++ 05-SceneManager/Card.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "GameObject.h"
+
+#define CARD_BBOX_WIDTH		16
+
+class CardItem : public CGameObject
+{
+public:
+	CardItem();
+	~CardItem();
+	virtual void Render();
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL);
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+
+};
